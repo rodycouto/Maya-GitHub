e04dad1f3006ca7137d52c5a456a5192aa9013d1
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-04-20 20:55:29 -0300

    updated

--- interacao/affect.js
+++ interacao/affect.js
@@ -4,27 +4,19 @@ const db = require('quick.db')
 
 exports.run = async (client, message, args) => {
 
-  if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) {
-    const adm = new Discord.MessageEmbed()
-      .setColor('#FF0000')
-      .setTitle('Eu preciso da permissão "Gerenciar Mensagens" para utilizar esta função.')
-    return message.inlineReply(adm)
-  }
-
   let prefix = db.get(`prefix_${message.guild.id}`)
   if (prefix === null) prefix = "-"
 
-  if (!args[0]) {
-    return message.inlineReply('`' + prefix + 'affect @user`')
-  }
-  const member = message.mentions.users.first() || message.author
+  if (!args[0]) { return message.inlineReply('`' + prefix + 'affect @user`') }
 
-  if (member.id === message.author.id) {
-    return message.inlineReply('Você não pode usar este comando com você mesmo.')
-  }
+  const member = message.mentions.users.first() || message.author;
+
+  if (member.id === message.author.id) { return message.inlineReply('Você não pode usar este comando com você mesmo.') }
+  if (member.id === "821471191578574888") { return message.inlineReply('Não usa isso comigo boboca.') }
+
   const memberAvatar = member.displayAvatarURL({ dynamic: false, format: 'png' })
-
   const image = await canvacord.affect(memberAvatar)
   const affect = new Discord.MessageAttachment(image, 'affect.png')
+
   return message.inlineReply(affect)
 }
