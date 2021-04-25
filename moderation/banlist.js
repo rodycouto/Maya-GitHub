0a96ca3349ada5a7bb1c80ad9d5767a488c93a02
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-04-24 23:38:33 -0300

    updated

---
+++ moderation/banlist.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js')
+const db = require('quick.db')
+
+exports.run = async (client, message, args) => {
+
+    if (!message.member.hasPermission("BAN_MEMBERS")) { return message.inlineReply(':x: Permissão Necessária: Banir Membros') }
+    if (!message.guild.me.hasPermission("BAN_MEMBERS")) { return message.inlineReply(':x: Eu preciso da permissão: "Banir Membros" para executar este comando.') }
+
+    message.guild.fetchBans().then(banned => {
+        let list = banned.map(user => user.tag).join('\n')
+
+        if (list.length >= 1950) list = `${list.slice(0, 1948)}...`
+
+        if (banned.size === null) { banned.size = '0' }
+
+        const embed = new Discord.MessageEmbed()
+            .setColor('BLUE')
+            .setTitle(`${banned.size} Usuários banidos!`)
+        if (list) { embed.setDescription(list) }
+        message.inlineReply(embed)
+    })
+
+}
