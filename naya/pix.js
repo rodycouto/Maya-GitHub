4949e8cb061d256b69ed8dd981f6d176651874e0
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-04-30 00:18:34 -0300

    updated

---
+++ naya/pix.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js')
+
+exports.run = async (client, message, args) => {
+
+    const LinkDoServidor = 'https://discord.gg/YpFWgJuuUV'
+    const LinkMercadoPago = 'https://mpago.la/2jYiNDg'
+    const ImagePix = 'https://imgur.com/0Xsb1n8'
+
+    const EmbedPix = new Discord.MessageEmbed()
+        .setColor('GREEN')
+        .setTitle('<:pix:837500432095248436> PIX Naya')
+        .setDescription(`Aqui você pode fazer PIX pra mim facilmente.\nSe precisar de qualquer coisa, entre no [meu servidor.](${LinkDoServidor})`)
+        .addField('<:ok:832667759796158504> Meios de PIX', `📨 Email: nayadiscordbot@hotmail.com\n<a:MoneyWings:834899137991540797> Pagamento Direto: [R$1,00 Mercado Pago](${LinkMercadoPago})\n📱 Via [QR Code](${ImagePix})`)
+        .setFooter('QUALQUER problema, entre em contato no servidor.')
+
+    return message.channel.send(EmbedPix)
+}
--- vip/vip.js
+++ vip/vip.js
@@ -14,8 +14,9 @@ exports.run = async (client, message, args) => {
         .setColor('#FDFF00')
         .setTitle(`${VipEmoji} VIP System Naya`)
         .setDescription(`*Antes de tudo, fique ciente de que o VIP System não dá previlégios ou vantagens a ninguém. O VIP System é uma forma de agradecimento e libera funções que não dão vantagens, apenas é legal tê-las.*`)
-        .addField(`❓ Como obter o VIP?`, `Simples, faça uma doação de [R$1,00](${link1Real}) ou dê uma ideia sensacional.\n` + '`' + prefix + 'donate` para mais informações')
-        .addField(`❓ O que eu ganho com o VIP?`, 'Os comandos VIPs estão sendo produzidos um a um. `' + prefix + 'esmola` e a Estrela 5 são muito bons.')
+        .addField(`❓ O que eu ganho com o VIP?`, 'Os comandos VIPs estão sendo produzidos um a um. `' + prefix + 'esmola` e a `-buy estrela5` são dois deles.')
+        .addField(`❓ Como obter o VIP?`, `Simples! Você pode fazer uma doação de [R$1,00](${link1Real}) no Mercado Pago ou fazer um PIX para o meu PicPay, basta digitar ` + '`' + prefix + 'pix`.\n' + '`' + prefix + 'donate` para mais informações')
+        .addField(`❓ Como comprovar o pagamento?`, `Simples! Entre no [meu servidor](${LinkServidor}) e fale com o **Rody#4191**, enviando um print do **comprovante** e pronto, você tem seu VIP.`)
         .addField('❓ Tem mais perguntas?', `Entre no [meu servidor](${LinkServidor}) e tire suas dúvidas`)
     return message.inlineReply(VipEmbed)
 }
